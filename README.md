99ae63fdcb0f976136fb8d089b1fc458e5e01dac
Neil Grogan <neil@grogan.org>
2026-05-27 15:16:27 +0100

    Start next part of course

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Has a few tracks:
 1. Introducton
 2. Basic Programming
 
+## Running examples
+
+```sh
+go run ./basic_programming
+go run ./func_and_oo
+```
--- basic_programming/operators.go
+++ basic_programming/operators.go
@@ -15,42 +15,42 @@ func Operators() {
 	fmt.Println(i, c, b)
 	Do_math()
 	Do_math_rel()
-	Do_math_bit() 
+	Do_math_bit()
 	Assign_op()
 	String_ptr()
 }
 
-func Do_math(){
+func Do_math() {
 	x := 5
 	y := 2
-	fmt.Println(x-y,x*y, x+y, x/y, x%y)
+	fmt.Println(x-y, x*y, x+y, x/y, x%y)
 }
 
-func Do_math_rel(){
+func Do_math_rel() {
 	x := 5
 	y := 2
-	fmt.Println(x==y,x!=y, x<y, x>y, x>=5)
+	fmt.Println(x == y, x != y, x < y, x > y, x >= 5)
 }
 
-func Do_math_log(){
+func Do_math_log() {
 	x := 5
 	y := 2
-	fmt.Println(x>y && x>=5)
-	fmt.Println(x>y || x<5)
+	fmt.Println(x > y && x >= 5)
+	fmt.Println(x > y || x < 5)
 	fmt.Println(!true, !false)
 }
 
-func Do_math_bit(){
+func Do_math_bit() {
 	// x := 5
 	// y := 2
-	fmt.Println("Bitshift 2 right: ", 2 << 1) // 0010 to 0100
-	fmt.Println("Bitshift 2 left: ", 2 >> 1) // 0010 to 0001
-	fmt.Println("2 and 1: ", 2 & 1) // 0010 & 0001 to 0000
-	fmt.Println("2 and not 1: ", 2 &^ 1) // 0010 &^ 0001 to 0010
-	fmt.Println("2 or 1: ", 2 | 1) // 0010 | 0001 to 0011
+	fmt.Println("Bitshift 2 right: ", 2<<1) // 0010 to 0100
+	fmt.Println("Bitshift 2 left: ", 2>>1)  // 0010 to 0001
+	fmt.Println("2 and 1: ", 2&1)           // 0010 & 0001 to 0000
+	fmt.Println("2 and not 1: ", 2&^1)      // 0010 &^ 0001 to 0010
+	fmt.Println("2 or 1: ", 2|1)            // 0010 | 0001 to 0011
 }
 
-func Assign_op(){
+func Assign_op() {
 	x := 5
 	y := 2
 	x += y
@@ -63,7 +63,7 @@ func Assign_op(){
 	fmt.Println("x/=y", x)
 }
 
-func String_ptr(){
+func String_ptr() {
 	x := "hello"
 	y := &x
 	fmt.Println(y)
