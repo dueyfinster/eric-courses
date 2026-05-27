9ef739a6160a18a5113e775d00b86f325f1c0f4e
Neil Grogan <neil@grogan.org>
2026-05-27 11:58:14 +0100

    finished variables

--- variables.go
+++ variables.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func Variables() {
+	var msg string = "a string"
+	fmt.Println("declared string", msg)
+
+	var msg2 string
+	fmt.Println("empty string", msg2)
+
+	var msg3 string
+	msg3 = "a string"
+	fmt.Println("declare after init string", msg3)
+
+	// type is inferred by compiler
+	msg4 := "a string"
+	fmt.Println("short declare string", msg4)
+
+	var x int
+	var truth bool
+	var msg5 string
+	var pointer *string
+	fmt.Println("integer: ", x)
+	fmt.Println("boolean: ", truth)
+	fmt.Println("string: ", msg5)
+	fmt.Println("pointer: ", pointer)
 
 }
