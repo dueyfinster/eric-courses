a90562a314c82d2500e90c6a71b625c0b8cd60d3
Neil Grogan <neil@grogan.org>
2026-05-29 13:51:01 +0100

    finished params to func

--- func_and_oo/params_to_func.go
+++ func_and_oo/params_to_func.go
@@ -2,6 +2,40 @@ package main
 
 import "fmt"
 
+// globals - shouldn't use
+// var x int = 20
+// var y int = 10
+
+// using individdual variables
+// func add(x, y, z int) int {
+// 	return x + y + z
+// }
+
+func add(x []int) int {
+	total := 0
+
+	for _, val := range x {
+		total += val
+	}
+	return total
+}
+
+func mul(x, y *int) int {
+	return *x * *y
+}
+
 func PassingParamsToFunc() {
-	fmt.Println()
+	// using individual values
+	// x := 20
+	// y := 10
+	// z := 5
+	//
+	// fmt.Println(add(x, y, z))
+
+	s := []int{10, 20, 5}
+	fmt.Println(add(s))
+
+	x := 5
+	y := 2
+	fmt.Println(mul(&x, &y))
 }
