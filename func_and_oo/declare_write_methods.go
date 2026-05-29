28ed8e80f13de098a46883dceec866156247ba5a
Neil Grogan <neil@grogan.org>
2026-05-29 16:26:42 +0100

    Finish declare and write methods in go

--- func_and_oo/declare_write_methods.go
+++ func_and_oo/declare_write_methods.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// Box is an exportavle object
+type Box struct {
+	D float64
+	W float64
+	H float64
+}
+
+// volume method to calculate volume of a box
+func (b *Box) volume() float64 {
+	return b.D * b.W * b.H
+}
+
 func DeclareAndWriteMethods() {
-	_ = fmt.Println
+	b := Box{D: 5, W: 4, H: 3}
+	fmt.Println(b.volume())
 }
--- func_and_oo/def_init_structs.go
+++ func_and_oo/def_init_structs.go
@@ -4,22 +4,22 @@ import "fmt"
 
 // Struct is lightweight named fields, weak OO
 // Box is an exportable object
-type Box struct {
+type Box1 struct {
 	D float64
 	W float64
 	H float64
 }
 
 func DefineAndInitStructs() {
-	b := Box{5, 4, 4}
+	b := Box1{5, 4, 4}
 	fmt.Println(b)
 
 	// using attribute names
-	c := Box{H: 5, W: 4, D: 4}
+	c := Box1{H: 5, W: 4, D: 4}
 	fmt.Println(c)
 
 	// changing values via attributes
-	d := Box{H: 5, W: 4, D: 4}
+	d := Box1{H: 5, W: 4, D: 4}
 	d.D = 6
 	fmt.Println(d)
 
