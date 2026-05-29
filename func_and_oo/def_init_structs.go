e5954fd8325b9dd4abd3a4d172a043933fa05531
Neil Grogan <neil@grogan.org>
2026-05-29 14:45:53 +0100

    finished define and init structs

--- func_and_oo/def_init_structs.go
+++ func_and_oo/def_init_structs.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+// Struct is lightweight named fields, weak OO
+// Box is an exportable object
+type Box struct {
+	D float64
+	W float64
+	H float64
+}
+
 func DefineAndInitStructs() {
-	_ = fmt.Println
+	b := Box{5, 4, 4}
+	fmt.Println(b)
+
+	// using attribute names
+	c := Box{H: 5, W: 4, D: 4}
+	fmt.Println(c)
+
+	// changing values via attributes
+	d := Box{H: 5, W: 4, D: 4}
+	d.D = 6
+	fmt.Println(d)
+
+	// access attributes via pointers with explicit dererference
+	ptr := &d
+	(*ptr).D = 7
+	fmt.Println(d)
+
+	// access attributes via pointers without explicit dererference
+	ptr2 := &d
+	ptr2.D = 9
+	fmt.Println(d)
 }
