7fc5b1b35c3ea1bb9ce8c0355b63c4897db85653
Neil Grogan <neil@grogan.org>
2026-05-29 16:50:09 +0100

    Finished course material part 2

--- func_and_oo/create_interact_objects.go
+++ func_and_oo/create_interact_objects.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+type Shape interface {
+	volume() float64
+}
+
+type Cube struct {
+	depth  float64
+	width  float64
+	height float64
+}
+
+func (cu *Cube) volume() float64 {
+	return cu.depth * cu.width * cu.height
+}
+
+type Sphere struct {
+	radius float64
+}
+
+// 4/3 * pi * r*r*r
+func (sp Sphere) volume() float64 {
+	return ((4.0 / 3.0) * 3.14 * (sp.radius * sp.radius * sp.radius))
+}
+
+func totalVolume(shapes ...Shape1) float64 {
+	var volume float64
+
+	for _, s := range shapes {
+		volume += s.volume()
+	}
+
+	return volume
+}
+
 func CreateAndInteractWithObjects() {
-	_ = fmt.Println
+	cu := Cube{depth: 4, width: 4, height: 4}
+	sp := Sphere{radius: 4}
+
+	// pass the pointer to cube and sphere object
+	fmt.Println("Total volume Obj:", totalVolume1(&cu, &sp))
 }
--- func_and_oo/oo.go
+++ func_and_oo/oo.go
@@ -6,7 +6,7 @@ import "fmt"
 // structs have named fields and nethods optionally
 func OO() {
 	// Objects
-	c := Cube{depth: 4, width: 4, height: 4}
+	c := Cube1{depth: 4, width: 4, height: 4}
 	fmt.Println("Cube vol:", c.volume())
 
 	// Inheritance
@@ -16,41 +16,41 @@ func OO() {
 	fmt.Println("Tyres:", t.Mfc())
 
 	// Interfaces
-	sp := Sphere{radius: 4}
-	var shapes []Shape
+	sp := Sphere1{radius: 4}
+	var shapes []Shape1
 
 	shapes = append(shapes,
 		c,
 		sp,
 	)
 
-	fmt.Println("Total volume:", totalVolume(shapes...))
+	fmt.Println("Total volume:", totalVolume1(shapes...))
 }
 
-type Cube struct {
+type Cube1 struct {
 	width  float64
 	height float64
 	depth  float64
 }
 
-type Sphere struct {
+type Sphere1 struct {
 	radius float64
 }
 
-type Shape interface {
+type Shape1 interface {
 	volume() float64
 }
 
-func (c Cube) volume() float64 {
+func (c Cube1) volume() float64 {
 	return c.depth * c.width * c.height
 }
 
 // 4/3 * pi * r*r*r
-func (sp Sphere) volume() float64 {
+func (sp Sphere1) volume() float64 {
 	return ((4.0 / 3.0) * 3.14 * (sp.radius * sp.radius * sp.radius))
 }
 
-func totalVolume(shapes ...Shape) float64 {
+func totalVolume1(shapes ...Shape1) float64 {
 	var volume float64
 
 	for _, s := range shapes {
