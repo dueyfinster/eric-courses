a51a88fa9ef12e84213d0ca2ee68ad072092a2f6
Neil Grogan <neil@grogan.org>
2026-05-29 16:37:33 +0100

    implemented interfaces

--- func_and_oo/working_interfaces.go
+++ func_and_oo/working_interfaces.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+// with interface we define methods
+type Food interface {
+	Nutrition() string
+	FoodType() string
+}
+
+type Apple struct {
+}
+
+func (a Apple) Nutrition() string {
+	return "Apples are carn heavy!"
+}
+
+func (a Apple) FoodType() string {
+	return "Apples are fruit"
+}
+
+type Celery struct {
+}
+
+func (c Celery) Nutrition() string {
+	return "Celery has zero everything!"
+}
+
+func (c Celery) FoodType() string {
+	return "Celery is a vergtable."
+}
+
 func WorkingWithInterfaces() {
-	_ = fmt.Println
+	foods := []Food{Apple{}, Celery{}}
+	for _, f := range foods {
+		fmt.Println(f.Nutrition(), f.FoodType())
+	}
 }
